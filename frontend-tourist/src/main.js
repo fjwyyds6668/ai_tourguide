b0fc4155ac6e310376e820bde7024bed39c29664
fjw <2941927554@qq.com>
2026-03-17 17:00:43 +0800

    添加 Element Plus 加载指令及样式，以支持加载状态的显示

--- frontend-tourist/src/main.js
+++ frontend-tourist/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { ElLoadingDirective } from 'element-plus'
+import 'element-plus/es/components/loading/style/css'
 import {
   Microphone,
   Location,
@@ -34,6 +36,7 @@ for (const [key, component] of Object.entries(icons)) {
 
 app.use(pinia)
 app.use(router)
+app.directive('loading', ElLoadingDirective)
 
 app.mount('#app')
 
